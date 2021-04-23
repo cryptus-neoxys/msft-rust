0afdd39c758f58944ddbc5e2f62f272707bc95da
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-24 05:00:22 +0530

    ✨ String Strings

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,5 +1,14 @@
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+
 fn main() {
-    let is_bigger: bool = 1 > 4;
     let heart_eyed_cat = '😻';
-    println!("boolean {} \nutf-8 {}", is_bigger, heart_eyed_cat);
+    let mut hello = String::from("Hello, ");
+    hello.push('w');
+    hello.push_str("orld!");
+    println!("&str: {}\nString: {}", heart_eyed_cat, hello);
+    print_type_of(&heart_eyed_cat);
+    print_type_of(&hello);
 }
